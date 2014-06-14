6d90197249ea4869c06e7c045c1d4fa93f981d31
Michael Bova <bovavob@gmail.com>
2014-06-13 21:42:16 -0500

    added numbered steps in print_cookbook method

--- cookbook/cookbook.rb
+++ cookbook/cookbook.rb
@@ -23,10 +23,17 @@ class Cookbook
 	end
 
 	def print_cookbook
-		@recipes.each do |x| 
+		@recipes.each do |x|
+			
 			print "Title: #{x.title}\n"
 			print "Ingredients: #{x.ingredients.join(', ')}\n"
-			print "Steps: #{x.steps.join("\n")}"
+			#print "Steps: #{x.steps.map { |k| "#{k.index}: #{k}" }.join("\n")}"
+			indexCount = 1
+			print "Steps:" + "\n"
+			x.steps.each do |v|
+				print "#{indexCount}: " + v + "\n"
+				indexCount += 1
+			end
 		end
 	end
 
